7274bbcc6592a6a1004daea97df72394ce88198e
Isabela Taborda <isabelataborda2005@gmail.com>
2025-02-25 00:59:49 -0500

    comentarios

--- directory-places/src/place-tags/place-tags.controller.ts
+++ directory-places/src/place-tags/place-tags.controller.ts
@@ -9,7 +9,7 @@ export class PlaceTagsController {
   constructor(private readonly placeTagsService: PlaceTagsService) {}
 
   /**
-   * ✅ Asigna una etiqueta a un lugar.
+   *  Asigna una etiqueta a un lugar.
    */
   @Post()
   create(@Body() createPlaceTagDto: CreatePlaceTagDto): Promise<PlaceTag> {
@@ -17,7 +17,7 @@ export class PlaceTagsController {
   }
 
   /**
-   * ✅ Obtiene todas las relaciones `PlaceTag`.
+   *  Obtiene todas las relaciones `PlaceTag`.
    */
   @Get()
   findAll(): Promise<PlaceTag[]> {
@@ -25,7 +25,7 @@ export class PlaceTagsController {
   }
 
   /**
-   * ✅ Elimina la relación entre un lugar y una etiqueta.
+   *  Elimina la relación entre un lugar y una etiqueta.
    */
   @Delete()
   remove(@Body() deletePlaceTagDto: DeletePlaceTagDto): Promise<void> {
--- directory-places/src/place-tags/place-tags.service.ts
+++ directory-places/src/place-tags/place-tags.service.ts
@@ -12,21 +12,21 @@ export class PlaceTagsService {
   ) {}
 
   /**
-   * ✅ Asigna una etiqueta a un lugar.
+   *  Asigna una etiqueta a un lugar.
    */
   async create(createPlaceTagDto: CreatePlaceTagDto): Promise<PlaceTag> {
     return this.placeTagRepository.create(createPlaceTagDto as any);
   }
 
   /**
-   * ✅ Obtiene todas las relaciones `PlaceTag`.
+   *  Obtiene todas las relaciones `PlaceTag`.
    */
   async findAll(): Promise<PlaceTag[]> {
     return this.placeTagRepository.findAll();
   }
 
   /**
-   * ✅ Elimina la relación entre un lugar y una etiqueta.
+   *  Elimina la relación entre un lugar y una etiqueta.
    */
   async remove(deletePlaceTagDto: DeletePlaceTagDto): Promise<void> {
     await this.placeTagRepository.destroy({
